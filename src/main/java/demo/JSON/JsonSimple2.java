14b8aaf5ddd9633558afa79f98407f7f46f6e8ca
bobo <bobo2008sap@gmail.com>
2019-10-17 16:22:07 +0800

    JAVA 学习 20191017

--- src/main/java/demo/JSON/JsonSimple2.java
+++ src/main/java/demo/JSON/JsonSimple2.java
@@ -5,8 +5,6 @@ import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
-
-
 import java.io.FileReader;
 import java.io.IOException;
 
